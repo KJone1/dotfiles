99f616e8116fefdbddf80d113723259bbe1a23d1
KJone1 <davidyahin@protonmail.com>
2024-02-25 22:32:53 +0200

    Add -d flag for debug

--- install.sh
+++ install.sh
@@ -2,4 +2,21 @@
 
 set -e
 
-stow . -t $HOME --ignore=install.sh
+# Initialize an empty options string
+OPTIONS=""
+
+while getopts "d" opt; do
+  case $opt in
+    d)
+      # If the -d (debug) flag is present, pass -n to stow
+      OPTIONS="-n"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+stow . -vv $OPTIONS -t $HOME --ignore=install.sh
+
