0230141db7cf7bbe905fd1d9100cb5a3b1948c49
KJone1 <davidyahin@protonmail.com>
2024-03-21 23:45:06 +0200

    Remvoe TextChanged event

--- .config/nvim/lua/plugins/auto-save.lua
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -1,7 +1,7 @@
 return {
   "okuuva/auto-save.nvim",
   cmd = "ASToggle", -- optional for lazy loading on command
-  event = { "InsertLeave", "BufLeave", "FocusLost", "TextChanged" }, -- optional for lazy loading on trigger events 
+  event = { "InsertLeave", "BufLeave", "FocusLost" }, -- optional for lazy loading on trigger events
   opts = {
     execution_message = {
       enabled = false,
