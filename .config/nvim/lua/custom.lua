eef7e05ab37627f9918e57dfa83619401a1f06e7
KJone1 <davidyahin@protonmail.com>
2024-03-23 22:23:17 +0200

    Minor improvments

--- .config/nvim/lua/custom.lua
+++ .config/nvim/lua/custom.lua
@@ -2,35 +2,42 @@ local M = {}
 
 function M.ssr_line()
 
+  local line = vim.api.nvim_get_current_line()
+
+  if line ==  "" then
+    vim.notify('Current line is blank', vim.log.levels.ERROR)
+    return
+  end
+
   local search = vim.fn.input("Search: ")
 
   if search == "" then
     return
   end
 
-  local line = vim.api.nvim_get_current_line()
+  local escaped_search = vim.fn.escape(search, '\\/.*$^~[]')
 
-  if vim.fn.search(search, 'cn', line) ~= 0 then
+  if vim.fn.search(escaped_search, 'cn', line) ~= 0 then
 
     vim.cmd('highlight Search ctermbg=NONE ctermfg=lightmagenta guibg=lightmagenta guifg=black')
 
     local line_number = vim.fn.line('.')
-    local escaped_search = vim.fn.escape(search, '\\/.*$^~[]')
 
     local pattern = '/\\%' .. line_number .. 'l' .. escaped_search .. '/'
     vim.cmd(string.format('match Search %s',pattern))
   else
-    vim.notify(string.format('Pattern "%s" not found on this line',search), vim.log.levels.ERROR)
+    vim.notify(string.format('Pattern "%s" not found on this line',escaped_search), vim.log.levels.ERROR)
     return
   end
 
   local replace = vim.fn.input("Replace: ")
   if replace == "" then
     vim.cmd('highlight clear Search')
+    vim.notify(string.format('Didnt replace "%s"',escaped_search), vim.log.levels.ERROR)
     return
   end
   vim.cmd('highlight clear Search')
-  vim.cmd(string.format('s/%s/%s/g', search, replace))
+  vim.cmd(string.format('s/%s/%s/g', escaped_search, replace))
 end
 function M.ssr_buffer()
 
@@ -55,6 +62,7 @@ function M.ssr_buffer()
   local replace = vim.fn.input("Replace: ")
   if replace == "" then
     vim.cmd('highlight clear Search')
+    vim.notify(string.format('Didnt replace "%s"',escaped_search), vim.log.levels.ERROR)
     return
   end
   vim.cmd('highlight clear Search')
