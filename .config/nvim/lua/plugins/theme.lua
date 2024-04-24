0f4078606a04f1eb1d2e64cf05b6c1363fbe8bf4
KJone1 <davidyahin@protonmail.com>
2024-04-24 17:54:54 +0300

    Update default theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -18,14 +18,27 @@ local themes = {
     priority = 1000,
   },
   {
+    'olivercederborg/poimandres.nvim',
+    lazy     = false,
+    priority = 1000,
+    event    = "User ColorSchemeLoad",
+    config   = function()
+      require('poimandres').setup {
+        bold_vert_split          = true,  -- use bold vertical separators
+        dim_nc_background        = true,  -- dim 'non-current' window backgrounds
+        disable_background       = false, -- disable background
+        disable_float_background = false, -- disable background for floats
+        disable_italics          = false, -- disable italics
+      }
+      vim.cmd.colorscheme("poimandres")
+    end,
+  },
+  {
     -- Default theme
     "Yazeed1s/oh-lucy.nvim",
-    lazy     = false,
+    lazy     = true,
     event    = "User ColorSchemeLoad",
     priority = 1000,
-    config   = function()
-      vim.cmd.colorscheme("oh-lucy")
-    end,
   },
 }
 
