2a6777c78b79377868e487ba98539d951061bc25
KJone1 <davidyahin@protonmail.com>
2024-04-25 21:02:23 +0300

    Move file_browser config into telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,26 +4,22 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-project.nvim',
-    -- Only load if `make` is available. Make sure you have the system
-    -- requirements installed.
+    "nvim-telescope/telescope-file-browser.nvim",
+    -- Only load if `make` is available. Make sure you have the system requirements installed.
     {
       'nvim-telescope/telescope-fzf-native.nvim',
-      -- NOTE: If you are having trouble with this installation,
-      -- refer to the README for telescope-fzf-native for more instructions.
       build = 'make',
       cond = function()
         return vim.fn.executable 'make' == 1
       end,
       config = function()
-        require('telescope').setup {
-          extensions = {
-          },
+        require("telescope").setup {
           defaults = {
-            theme = "center",
-            sorting_strategy = "ascending",
-            results_title = false,
-            prompt_prefix = "  ",
-            selection_caret = " ",
+            theme                = "center",
+            sorting_strategy     = "ascending",
+            results_title        = false,
+            prompt_prefix        = "  ",
+            selection_caret      = " ",
             file_ignore_patterns = {".git$"},
             mappings = {
               i = {
@@ -32,10 +28,50 @@ return {
               },
             },
           },
+          pickers = {
+            live_grep = {
+              vimgrep_arguments = {
+                "rg",
+                "--color=never",
+                "--no-heading",
+                "--column",
+                "--smart-case",
+                "--trim",
+                "--hidden",
+                "--glob",
+                "!**/.git/*"
+              },
+            },
+          },
+          extensions = {
+            file_browser = {
+              layout_config = {
+                width = 0.65,
+              },
+              previewer        = false,
+              hijack_netrw     = true,
+              grouped          = true,
+              auto_depth       = true,
+              prompt_path      = true,
+              select_buffer    = true,
+              display_stat     = false,
+              dir_icon         = " ",
+              git_status       = true,
+              hidden = {
+                file_browser   = true,
+                folder_browser = false,
+              },
+              mappings = {
+                ["i"] = {
+                  ["<bs>"]     = false  -- disable <bs> from going back to parent dir.
+                },
+              },
+            },
+          },
         }
-
-      require("telescope").load_extension('harpoon')
-      -- Enable telescope fzf native, if installed
+      -- Enable telescope extensions, if installed
+      pcall(require("telescope").load_extension,"harpoon")
+      pcall(require("telescope").load_extension,"file_browser")
       pcall(require('telescope').load_extension,'fzf')
       end
     },
