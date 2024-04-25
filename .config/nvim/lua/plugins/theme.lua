3a31c53aba6f731f8738df3b099c7f6b457942b3
KJone1 <davidyahin@protonmail.com>
2024-04-25 18:32:16 +0300

    Add nightfox theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -12,6 +12,20 @@ local themes = {
     priority = 1000,
   },
   {
+    "EdenEast/nightfox.nvim",
+    lazy     = false,
+    event    = "User ColorSchemeLoad",
+    priority = 1000,
+    config   = function()
+      require('nightfox').setup({
+        options = {
+          transparent = true
+        }
+      })
+      vim.cmd.colorscheme("carbonfox")
+    end
+  },
+  {
     "getomni/neovim",
     lazy     = true,
     event    = "User ColorSchemeLoad",
@@ -19,19 +33,19 @@ local themes = {
   },
   {
     'olivercederborg/poimandres.nvim',
-    lazy     = false,
+    lazy     = true,
     priority = 1000,
     event    = "User ColorSchemeLoad",
-    config   = function()
-      require('poimandres').setup {
-        bold_vert_split          = true,  -- use bold vertical separators
-        dim_nc_background        = true,  -- dim 'non-current' window backgrounds
-        disable_background       = false, -- disable background
-        disable_float_background = false, -- disable background for floats
-        disable_italics          = false, -- disable italics
-      }
-      vim.cmd.colorscheme("poimandres")
-    end,
+    -- config   = function()
+    --   require('poimandres').setup {
+    --     bold_vert_split          = true,  -- use bold vertical separators
+    --     dim_nc_background        = true,  -- dim 'non-current' window backgrounds
+    --     disable_background       = false, -- disable background
+    --     disable_float_background = false, -- disable background for floats
+    --     disable_italics          = false, -- disable italics
+    --   }
+    --   vim.cmd.colorscheme("poimandres")
+    -- end,
   },
   {
     -- Default theme
