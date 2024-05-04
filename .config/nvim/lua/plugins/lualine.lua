4773fd5203dd55f64747ef6bfe5efcf91465524d
KJone1 <davidyahin@protonmail.com>
2024-05-04 21:17:15 +0300

    Update lualine components

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -25,57 +25,54 @@ return {
             },
             {
               'diff',
-              symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
+              symbols = { added = ' ', modified = ' ', removed = ' ' },
               color = {
                 bg  = "NONE",
               },
               diff_color = {
                 added    = { fg = '#98be65' },
                 modified = { fg = '#FF8800' },
-                removed  = { fg = '#ec5f67' },
+                removed  = { fg = '#ca1243' },
               },
             },
           },
-          lualine_c = {
-          {
-            'diagnostics',
-            source            = { 'nvim' },
-            sections          = { 'error' },
-            color             = { bg  = "NONE" },
-            separator         = { left = ' ' },
-            diagnostics_color = { error = { bg = '#ca1243', fg = '#f3f3f3' } },
-          },
-          {
-            'diagnostics',
-            source            = { 'nvim' },
-            sections          = { 'warn' },
-            color             = { bg  = "NONE" },
-            separator         = { left = ' '},
-            diagnostics_color = { warn = { bg = '#fe8019', fg = '#f3f3f3' } },
-          },
-          {
-            '%w',
-            color = { bg  = "NONE" },
-            cond  = function()
-              return vim.wo.previewwindow
-            end,
-          },
-          {
-            '%r',
-            color = { bg  = "NONE" },
-            cond  = function()
-              return vim.bo.readonly
-            end,
-          },
-          {
-            '%q',
-            color = { bg  = "NONE" },
-            cond  = function()
-              return vim.bo.buftype == 'quickfix'
-            end,
-          },
-          },
+          lualine_c = {},
           lualine_x = {
+            {
+              'diagnostics',
+              source            = { 'nvim' },
+              sections          = { 'error' },
+              separator         = { left = ' ' },
+              diagnostics_color = { error = { bg = 'NONE', fg = '#ca1243' } },
+            },
+            {
+              'diagnostics',
+              source            = { 'nvim' },
+              sections          = { 'warn' },
+              separator         = { left = ' '},
+              diagnostics_color = { warn = { bg = 'NONE', fg = '#FF8800' } },
+            },
+            {
+              '%w',
+              color = { bg  = "NONE" },
+              cond  = function()
+                return vim.wo.previewwindow
+              end,
+            },
+            {
+              '%r',
+              color = { bg  = "NONE" },
+              cond  = function()
+                return vim.bo.readonly
+              end,
+            },
+            {
+              '%q',
+              color = { bg  = "NONE" },
+              cond  = function()
+                return vim.bo.buftype == 'quickfix'
+              end,
+            },
             {
               require("custom").harpoon_component,
               color = {
