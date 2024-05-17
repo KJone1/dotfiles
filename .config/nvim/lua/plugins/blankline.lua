b074da7c130b6d8229db528069053466f630fe8c
KJone1 <davidyahin@protonmail.com>
2024-05-17 22:42:58 +0300

    Fix indent

--- .config/nvim/lua/plugins/blankline.lua
+++ .config/nvim/lua/plugins/blankline.lua
@@ -1,26 +1,26 @@
 return{
-   'lukas-reineke/indent-blankline.nvim',
-   -- See `:help indent_blankline.txt`
-   main = "ibl",
-   opts = {
-      indent = {
-         char = "▏",
-         smart_indent_cap = false
-      },
-      whitespace = { highlight = { "Whitespace", "NonText" } },
-      exclude = {
-         filetypes = {
-           "lspinfo",
-           "packer",
-           "checkhealth",
-           "help",
-           "man",
-           "gitcommit",
-           "TelescopePrompt",
-           "TelescopeResults",
-           "''",
-           "dashboard"
-         }
+  'lukas-reineke/indent-blankline.nvim',
+  -- See `:help indent_blankline.txt`
+  main = "ibl",
+  opts = {
+    indent = {
+       char = "▏",
+       smart_indent_cap = false
+    },
+    whitespace = { highlight = { "Whitespace", "NonText" } },
+    exclude = {
+      filetypes = {
+       "lspinfo",
+       "packer",
+       "checkhealth",
+       "help",
+       "man",
+       "gitcommit",
+       "TelescopePrompt",
+       "TelescopeResults",
+       "''",
+       "dashboard"
       }
-   },
+    }
+  },
 }
