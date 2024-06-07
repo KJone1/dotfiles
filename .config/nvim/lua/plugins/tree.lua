ff46c34a7cb8a72aad3d3be41095c1f064cda6cb
KJone1 <davidyahin@protonmail.com>
2024-06-07 10:12:28 +0300

    Formatting

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -1,49 +1,53 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
   },
   config = function()
-    require("neo-tree").setup({
+    require('neo-tree').setup {
       close_if_last_window = true,
       enable_diagnostics = true,
       default_component_configs = {
         icon = {
-          folder_closed = " ",
-          folder_open = " ",
-          folder_empty = "󱧵 ",
-          default = "",
+          folder_closed = ' ',
+          folder_open = ' ',
+          folder_empty = '󱧵 ',
+          default = '',
         },
         git_status = {
           symbols = {
             -- Change type
-            added     = " ",
-            modified  = " ",
-            deleted   = " ",
-            renamed   = "  ",
+            added = ' ',
+            modified = ' ',
+            deleted = ' ',
+            renamed = '  ',
             -- Status type
-            untracked = "",
-            ignored   = " ",
-            unstaged  = "",
-            staged    = " ",
-            conflict  = " ",
-          }
+            untracked = '',
+            ignored = ' ',
+            unstaged = '',
+            staged = ' ',
+            conflict = ' ',
+          },
         },
       },
       window = {
-        position = "float",
+        position = 'float',
       },
       filesystem = {
         filtered_items = {
           hide_dotfiles = false,
           hide_by_name = {
-            ".git"
-          }
-        }
-      }
-    })
-  end
+            '.git',
+          },
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = true,
+          },
+        },
+      },
+    }
+  end,
 }
