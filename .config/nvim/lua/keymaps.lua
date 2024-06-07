2c3efcc81b026749732643bb9267b58069710ac8
KJone1 <davidyahin@protonmail.com>
2024-06-07 10:14:16 +0300

    Disable F1 help keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -4,6 +4,7 @@ M.general = {
   n = { -- Normal mode mappings
     ['[d']        = { vim.diagnostic.goto_prev,'Go to previous diagnostic message' },
     [']d']        = { vim.diagnostic.goto_next, 'Go to next diagnostic message' },
+    ['<F1>'] = { '<ESC>', 'Remap F1 as escape' },
   },
   i = { -- Insert mode mappings
   },
