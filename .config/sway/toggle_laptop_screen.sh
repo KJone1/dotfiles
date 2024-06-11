03e0700a913ee8386608c0da2691360374b2a647
KJone1 <davidyahin@protonmail.com>
2024-06-11 22:55:49 +0300

    Made sure the script doesnt run if external monitor is disconnected

--- .config/sway/toggle_laptop_screen.sh
+++ .config/sway/toggle_laptop_screen.sh
@@ -2,9 +2,16 @@
 
 set -eo pipefail
 
-laptop_display=$(swaymsg -t get_outputs | jq -r '[.[] | select(.name | test("HDMI"; "i") | not) | .name] | join("\n")')
+has_multiple_displays() {
+  result=$(swaymsg -t get_outputs | jq '. | length > 1')
+  [[ "$result" == "true" ]]
+}
 
-echo "Toggling output ${laptop_display}"
+laptop_display=$(swaymsg -t get_outputs | jq -r '[.[] | select(.name | test("eDP"; "i")) | .name] | join("\n")')
 
-swaymsg output "${laptop_display}" toggle
-
+if has_multiple_displays; then
+  echo "Toggling output: ${laptop_display}"
+  swaymsg output "${laptop_display}" toggle
+else
+  echo "No external display connected. No toggling needed."
+fi
