7d2e0254f4fd5b845b55a908fab4ae54666b0fe0
KJone1 <davidyahin@protonmail.com>
2024-06-29 14:25:45 +0300

    Use cinnamon for scrolling instead of neoscroll

--- .config/nvim/lua/plugins/neoscroll.lua
+++ .config/nvim/lua/plugins/neoscroll.lua
@@ -1,6 +1,17 @@
 return {
-  "karb94/neoscroll.nvim",
-  config = function ()
-    require('neoscroll').setup {}
-  end
+  'declancm/cinnamon.nvim',
+  config = function()
+    require('cinnamon').setup {
+      keymaps = {
+        basic = true,
+        extra = false,
+      },
+      options = {
+        max_delta = {
+          time = 2000,
+        },
+        mode = 'cursor',
+      },
+    }
+  end,
 }
