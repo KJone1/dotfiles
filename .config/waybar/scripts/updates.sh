92c258469df8c4963d8bd6739057538e0bdc8d74
KJone1 <davidyahin@protonmail.com>
2024-07-12 21:22:40 +0300

    Make updates script more robust

--- .config/waybar/scripts/updates.sh
+++ .config/waybar/scripts/updates.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-update_count=$(( $(dnf check-update | grep -cE ' updates$') ))  # Count or set to 0
+update_count=$(( $(dnf list updates -q | awk '{ print $2 }' | grep -cE '[0-9]+') ))  # Count or set to 0
 
 if [ "$update_count" -gt 0 ]; then
   echo "$update_count"
