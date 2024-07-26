7df2f365c5b1f1a3018680921d4468aeace795a1
KJone1 <davidyahin@protonmail.com>
2024-07-26 13:31:41 +0300

    Formatting

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,18 +1,18 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
+  'folke/noice.nvim',
+  event = 'VeryLazy',
   opts = {},
   dependencies = {
-    "MunifTanjim/nui.nvim",
+    'MunifTanjim/nui.nvim',
   },
   config = function()
-    require("noice").setup({
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"]                = true,
-            ["cmp.entry.get_documentation"]                  = true,
+    require('noice').setup {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
         },
       },
       notify = {
@@ -20,11 +20,11 @@ return {
       },
       -- you can enable a preset for easier configuration
       presets = {
-        bottom_search         = false, -- use a classic bottom cmdline for search
-        command_palette       = true,  -- position the cmdline and popupmenu together
-        long_message_to_split = true,  -- long messages will be sent to a split
-        inc_rename            = false, -- enables an input dialog for inc-rename.nvim
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
       },
-    })
-  end
+    }
+  end,
 }
