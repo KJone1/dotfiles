4d2da249d7a16a9cd41e5547b3a8a2416b82a96c
KJone1 <davidyahin@protonmail.com>
2024-08-10 20:52:42 +0300

    Init conform.lua

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -26,26 +26,6 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    opts = {
-      notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
-      },
-    },
-  },
-
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
