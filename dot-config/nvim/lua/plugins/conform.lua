51067047706f3d6a8494d94e27d28b2922b8bf1b
KJone1 <davidyahin@protonmail.com>
2024-08-10 20:58:40 +0300

    Update: lazy load conform.nvim

--- dot-config/nvim/lua/plugins/conform.lua
+++ dot-config/nvim/lua/plugins/conform.lua
@@ -1,13 +1,32 @@
 return {
   'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  lazy = true,
+  ---@module "conform"
+  ---@type conform.setupOpts
   opts = {
     notify_on_error = false,
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
     formatters_by_ft = {
       lua = { 'stylua' },
+      python = { 'isort', 'black' },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+    },
+    default_format_opts = {
+      lsp_format = 'fallback',
+    },
+    format_on_save = {
+      lsp_fallback = true,
+      timeout_ms = 500,
+    },
+
+    formatters = {
+      shfmt = {
+        prepend_args = { '-i', '2' },
+      },
     },
   },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
