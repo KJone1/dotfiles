d7c4ef8b3c9ff003b61d54a3e2d48a8146b3d930
KJone1 <davidyahin@protonmail.com>
2024-09-07 13:51:47 +0300

    Add: flake8 as python linter

--- dot-config/nvim/lua/plugins/lsp/nvim-lint.lua
+++ dot-config/nvim/lua/plugins/lsp/nvim-lint.lua
@@ -11,6 +11,7 @@ return {
       sh = { 'shellcheck' },
       c = { 'cpplint' },
       cpp = { 'cpplint' },
+      python = { 'flake8' },
     }
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 
