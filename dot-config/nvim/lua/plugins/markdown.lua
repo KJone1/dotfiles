c8fc7a2139e6e79335b77b6fce46f52246027454
KJone1 <davidyahin@protonmail.com>
2024-09-07 13:53:26 +0300

    Rename and update markdown plugins

---
+++ dot-config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,17 @@
+local M = {
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ft = { 'markdown' },
+    opts = {},
+  },
+}
+return M
