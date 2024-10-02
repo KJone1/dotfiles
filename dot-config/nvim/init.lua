504422d846eb8f942a740467e60e58d281b902dc
KJone1 <davidyahin@protonmail.com>
2024-10-02 13:50:44 +0300

    Add lazy.nvim keymap to generate plugin README

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -30,7 +30,53 @@ local plugins = {
   { import = 'plugins' },
   { import = 'plugins.lsp' },
 }
-local opts = {}
+local opts = {
+  ui = {
+    custom_keys = {
+      ['<leader>r'] = {
+        function(_)
+          ---@type LazyPlugin[]
+          local plugin_list = require('lazy.core.config').plugins
+          local file_content = {
+            '## 💤 Plugin manager',
+            '',
+            '[lazy.nvim](https://github.com/folke/lazy.nvim)',
+            '',
+            '## 🔌 Plugins',
+            '',
+          }
+          local plugins_md = {}
+          for plugin, spec in pairs(plugin_list) do
+            if spec.url then
+              table.insert(plugins_md, ('- [%s](%s)'):format(plugin, spec.url:gsub('%.git$', '')))
+            end
+          end
+          table.sort(plugins_md, function(a, b)
+            return a:lower() < b:lower()
+          end)
+
+          for _, p in ipairs(plugins_md) do
+            table.insert(file_content, p)
+          end
+
+          table.insert(file_content, '')
+
+          local dotdir = os.getenv 'PWD'
+          local file_path = dotdir .. '/dot-config/nvim/README.md'
+
+          local file, err = io.open(file_path, 'w')
+          if not file then
+            error(err)
+          end
+          file:write(table.concat(file_content, '\n'))
+          file:close()
+          vim.notify('README.md succesfully generated', vim.log.levels.INFO, {})
+        end,
+        desc = 'Generate README.md file',
+      },
+    },
+  },
+}
 
 require('lazy').setup(plugins, opts)
 
