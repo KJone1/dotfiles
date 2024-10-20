0f2587697777df8c2fbc687840ce89bb3da66233
KJone1 <davidyahin@protonmail.com>
2024-10-20 20:29:08 +0300

    Add write_all_buffs for autosave plugin

--- dot-config/nvim/lua/plugins/auto-save.lua
+++ dot-config/nvim/lua/plugins/auto-save.lua
@@ -1,10 +1,11 @@
 return {
-  "okuuva/auto-save.nvim",
-  cmd = "ASToggle", -- optional for lazy loading on command
-  event = { "InsertLeave", "BufLeave", "FocusLost" }, -- optional for lazy loading on trigger events
+  'okuuva/auto-save.nvim',
+  cmd = 'ASToggle',
+  event = { 'InsertLeave', 'BufLeave', 'FocusLost' }, -- optional for lazy loading on trigger events
   opts = {
+    write_all_buffers = true,
     execution_message = {
       enabled = false,
-    }
+    },
   },
 }
