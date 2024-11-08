cdb7a19ab83cf083edd5edf87297b56c505ba1b2
KJone1 <davidyahin@protonmail.com>
2024-11-08 20:24:48 +0200

    Init snipe plugin for nvim

---
+++ dot-config/nvim/lua/plugins/snipe.lua
@@ -0,0 +1,19 @@
+return {
+  'leath-dub/snipe.nvim',
+  lazy = true,
+  hints = {
+    dictionary = 'sadflewcmpghio',
+  },
+  opts = {
+    sort = 'last',
+    navigate = {
+      close_buffer = 'dd',
+    },
+    ui = {
+      position = 'center',
+      open_win_override = {
+        border = 'rounded',
+      },
+    },
+  },
+}
