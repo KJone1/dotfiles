77ee61a11f8285cdea585f1b443ed1adf64f2723
KJone1 <davidyahin@protonmail.com>
2024-11-16 01:31:34 +0200

    Update todo comments opts

--- dot-config/nvim/lua/plugins/todo-comments.lua
+++ dot-config/nvim/lua/plugins/todo-comments.lua
@@ -1,5 +1,41 @@
 return {
   'folke/todo-comments.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = {},
+  opts = {
+    keywords = {
+      FIX = {
+        icon = ' ',
+        color = 'error',
+        alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' },
+      },
+      TODO = {
+        icon = ' ',
+        color = 'info',
+        alt = { 'todo' },
+      },
+      HACK = {
+        icon = ' ',
+        color = 'warning',
+      },
+      WARN = {
+        icon = ' ',
+        color = 'warning',
+        alt = { 'WARNING', 'XXX' },
+      },
+      PERF = {
+        icon = ' ',
+        alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' },
+      },
+      NOTE = {
+        icon = ' ',
+        color = 'hint',
+        alt = { 'INFO' },
+      },
+      TEST = {
+        icon = '⏲ ',
+        color = 'test',
+        alt = { 'TESTING', 'PASSED', 'FAILED' },
+      },
+    },
+  },
 }
