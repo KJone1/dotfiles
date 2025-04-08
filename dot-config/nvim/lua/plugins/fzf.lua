53ca3044109d53d5e04d552bc8bf4217a4be0674
KJone1 <davidyahin@protonmail.com>
2025-04-08 12:45:29 +0300

    Update fzf config for commands subscommand

--- dot-config/nvim/lua/plugins/fzf.lua
+++ dot-config/nvim/lua/plugins/fzf.lua
@@ -3,9 +3,16 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   cmd = 'FzfLua',
   config = function()
+    local actions = require('fzf-lua').actions
     require('fzf-lua').setup {
       files = {
         previewer = false,
+      },
+      commands = {
+        previewer = false,
+        actions = {
+          ['enter'] = actions.ex_run_cr,
+        },
       },
     }
   end,
