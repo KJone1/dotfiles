6ae34c4590deff1ce60355e2db90d3040e6c241e
KJone1 <davidyahin@protonmail.com>
2025-09-19 11:10:41 +0300

    Remove: unused render-markdown plugin

--- dot-config/nvim/lua/plugins/markdown.lua
+++ dot-config/nvim/lua/plugins/markdown.lua
@@ -1,21 +1,8 @@
-local M = {
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = function()
-      vim.fn['mkdp#util#install']()
-    end,
-  },
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-    ft = { 'markdown' },
-    opts = {
-      bullet = {
-        icons = { '󰇊 ', '󰇋 ', '󰇌 ', '󰇍 ', '󰇎 ', '󰇏 ' },
-      },
-    },
-  },
+return {
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
 }
-return M
