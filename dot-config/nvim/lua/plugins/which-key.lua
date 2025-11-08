aea42f5d54f118726d2ab7696b0bb14dfee874eb
KJone1 <david.yakhin@placer.ai>
2025-11-08 13:43:33 +0200

    Refactor which-key to use native keymap registration

--- dot-config/nvim/lua/plugins/which-key.lua
+++ dot-config/nvim/lua/plugins/which-key.lua
@@ -1,23 +1,27 @@
 return {
   'folke/which-key.nvim',
-  event = 'VeryLazy',
+  lazy = false,
+  priority = 1100,
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
-  end,
-  opts = {},
-  config = function()
-    local wk = require 'which-key'
-    local mappings = require 'keymaps'
 
-    local setupMappings = function()
-      for _, contextMappings in pairs(mappings) do
-        for mode, mapping in pairs(contextMappings) do
-          wk.add { mapping, mode = { mode } }
+    local mappings = require 'keymaps'
+    for _, contextMappings in pairs(mappings) do
+      for mode, keymaps in pairs(contextMappings) do
+        for _, keymap in ipairs(keymaps) do
+          local key = keymap[1]
+          local action = keymap[2]
+          local opts = { desc = keymap.desc, silent = true }
+          vim.keymap.set(mode, key, action, opts)
         end
       end
     end
-
-    setupMappings()
   end,
+  opts = {
+    preset = 'modern',
+    icons = {
+      mappings = false,
+    },
+  },
 }
