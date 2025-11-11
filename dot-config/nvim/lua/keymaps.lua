a49acf5e4177163a5e2c81d91d43e9b5928b4525
KJone1 <david.yakhin@placer.ai>
2025-11-11 10:15:00 +0200

    Refactor keymaps to flat structure

--- dot-config/nvim/lua/keymaps.lua
+++ dot-config/nvim/lua/keymaps.lua
@@ -1,105 +1,51 @@
-local M = {}
+return {
+  n = {
+    -- Diagnostics
+    { '[d', function() vim.diagnostic.jump { count = -1, float = true } end, desc = 'Go to previous diagnostic message' },
+    { ']d', function() vim.diagnostic.jump { count = 1, float = true } end, desc = 'Go to next diagnostic message' },
 
-M.general = {
-  n = { -- Normal mode mappings
-    { '[d', vim.diagnostic.jump { count = -1, float = true }, desc = 'Go to previous diagnostic message' },
-    { ']d', vim.diagnostic.jump { count = 1, float = true }, desc = 'Go to next diagnostic message' },
-    { '<leader>e', ':Neotree toggle reveal_force_cwd<CR>', desc = 'Explore Files in current dir' },
-    {
-      '<leader>j',
-      function()
-        require('snipe').open_buffer_menu()
-      end,
-      desc = 'Open Snipe buffer menu',
-    },
+    -- Files & Navigation
+    { '<leader>e', ':Yazi<CR>', desc = 'Explore Files in current dir' },
+    { '<leader>j', function() require('snipe').open_buffer_menu() end, desc = 'Open Snipe buffer menu' },
     { '<F1>', '<ESC>', desc = 'Remap F1 as escape' },
-  },
-  i = { -- Insert mode mappings
-    { '<F1>', '<ESC>', desc = 'Remap F1 as escape' },
-  },
-  v = { -- Visual mode mappings
-  },
-}
 
-local c = require 'cinnamon'
-M.scroll = {
-  n = {
-    {
-      '<C-k>',
-      function()
-        c.scroll '10kzz'
-      end,
-      desc = 'Remap Ctrl-u',
-    },
-    {
-      '<C-j>',
-      function()
-        c.scroll '10jzz'
-      end,
-      desc = 'Remap Ctrl-d',
-    },
-  },
-  i = {},
-  v = {
-    {
-      '<C-k>',
-      function()
-        c.scroll '10kzz'
-      end,
-      desc = 'Remap Ctrl-u',
-    },
-    {
-      '<C-j>',
-      function()
-        c.scroll '10jzz'
-      end,
-      desc = 'Remap Ctrl-d',
-    },
-  },
-}
+    -- Scroll
+    { '<C-k>', function() require('cinnamon').scroll '10kzz' end, desc = 'Remap Ctrl-u' },
+    { '<C-j>', function() require('cinnamon').scroll '10jzz' end, desc = 'Remap Ctrl-d' },
 
-M.git = {
-  n = {
-    { '<leader>l', ':LazyGit<cr>', desc = 'Open LazyGit', icon = { icon = '󰊢 ' } },
-  },
-}
+    -- Git
+    { '<leader>l', function() require('snacks').lazygit() end, desc = 'Open LazyGit' },
 
-M.ssr = {
-  n = {
-    {
-      '<leader>sl',
-      function()
-        require('custom').ssr_line()
-      end,
-      desc = 'Search and Replace in line',
-    },
-    {
-      '<leader>sa',
-      function()
-        require('custom').ssr_buffer()
-      end,
-      desc = 'Search and Replace in current buffer',
-    },
-  },
-}
+    -- Search/Replace
+    { '<leader>sl', function() require('custom').ssr_line() end, desc = 'Search and Replace in line' },
+    { '<leader>sa', function() require('custom').ssr_buffer() end, desc = 'Search and Replace in current buffer' },
 
-M.text = {
-  n = {
+    -- Text Editing
     { '<A-DOWN>', ':m .+1<CR>==', desc = 'Move one line down' },
     { '<A-UP>', ':m .-2<CR>==', desc = 'Move one line up' },
     { '<S-A-DOWN>', ':t.<CR>', desc = 'Duplicate line' },
     { '<leader>/', '<Cmd>lua require("Comment.api").toggle.linewise.current()<CR>', desc = 'Comment linewise' },
     { '<leader>md', ':MarkdownPreview<CR>', desc = 'Open Markdown Preview' },
+
+    -- LSP
+    { '<F2>', vim.lsp.buf.rename, desc = 'Refactor Rename' },
+
+    -- Fuzzy Finder
+    { '<leader>t', ':TodoFzfLua keywords=TODO,FIX<CR>', desc = 'List TODOs' },
+    { '<leader>fg', ':FzfLua live_grep_native resume=true<CR>', desc = 'Live grep Project' },
+    { '<leader>ff', ':FzfLua lgrep_curbuf resume=true<CR>', desc = 'Live grep current buffer' },
+    { '<leader>fw', ':FzfLua git_status<CR>', desc = 'Search modified files' },
+    { '<leader>fr', ':FzfLua oldfiles<CR>', desc = 'Search file history' },
+    { '<leader><space>', ':FzfLua files<CR>', desc = 'Search files in project' },
+    { ';', ':FzfLua commands<CR>', desc = 'Open commands list using fzf' },
+    { '<leader>cs', ':FzfLua colorschemes<CR>', desc = 'Explore colorschemes' },
   },
-  v = {
-    { '<A-UP>', ":m '<-2<CR>gv=gv", desc = 'Move selected lines up' },
-    { '<A-DOWN>', ":m '>+1<CR>gv=gv", desc = 'Move selected lines down' },
-    { '<leader>/', '<ESC><Cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', desc = 'Comment linewise selected text' },
-    { '<S-A-DOWN>', ":t'>\\<CR>gv=gv", desc = 'Duplicate line' },
-    { '<Tab>', '>gv', desc = 'Indent' },
-    { '<S-Tab>', '<gv', desc = 'Outdent' },
-  },
+
   i = {
+    -- General
+    { '<F1>', '<ESC>', desc = 'Remap F1 as escape' },
+
+    -- Text Manipulation
     { '<A-DOWN>', '<C-o>:m .+1<CR>', desc = 'Move one line down' },
     { '<A-UP>', '<C-o>:m .-2<CR>', desc = 'Move one line up' },
     { '<C-s>', '<C-o>:w<CR>', desc = 'Save' },
@@ -111,24 +57,18 @@ M.text = {
     { '<S-Tab>', '<C-d>', desc = 'Outdent line' },
     { '<Tab>', '<C-t>', desc = 'Indent line' },
   },
-}
 
-M.lsp = {
-  n = {
-    { '<F2>', vim.lsp.buf.rename, desc = 'Refactor Rename' },
+  v = {
+    -- Scroll
+    { '<C-k>', function() require('cinnamon').scroll '10kzz' end, desc = 'Remap Ctrl-u' },
+    { '<C-j>', function() require('cinnamon').scroll '10jzz' end, desc = 'Remap Ctrl-d' },
+
+    -- Text Manipulation
+    { '<A-UP>', ":m '<-2<CR>gv=gv", desc = 'Move selected lines up' },
+    { '<A-DOWN>', ":m '>+1<CR>gv=gv", desc = 'Move selected lines down' },
+    { '<leader>/', '<ESC><Cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', desc = 'Comment linewise selected text' },
+    { '<S-A-DOWN>', ":t'>\\<CR>gv=gv", desc = 'Duplicate line' },
+    { '<Tab>', '>gv', desc = 'Indent' },
+    { '<S-Tab>', '<gv', desc = 'Outdent' },
   },
 }
-
-M.telescope = {
-  n = {
-    { '<leader>t', ':TodoFzfLua keywords=TODO,FIX<CR>', desc = 'List TODOs' },
-    { '<leader>fg', ':FzfLua live_grep_native resume=true<CR>', desc = 'Live grep Project', icon = { icon = ' ' } },
-    { '<leader>ff', ':FzfLua lgrep_curbuf resume=true<CR>', desc = 'Live grep current buffer', icon = { icon = ' ' } },
-    { '<leader>fw', ':FzfLua git_status<CR>', desc = 'Search modified files', icon = { icon = ' ' } },
-    { '<leader>fr', ':FzfLua oldfiles<CR>', desc = 'Search file history', icon = { icon = ' ' } },
-    { '<leader><space>', ':FzfLua files<CR>', desc = 'Search files in project' },
-    { ';', ':FzfLua commands<CR>', desc = 'Open commands list using fzf' },
-    { '<leader>cs', ':FzfLua colorschemes<CR>', desc = 'Explore colorschemes' },
-  },
-}
-return M
--- dot-config/nvim/lua/plugins/which-key.lua
+++ dot-config/nvim/lua/plugins/which-key.lua
@@ -7,19 +7,14 @@ return {
     vim.o.timeoutlen = 300
 
     local mappings = require 'keymaps'
-    for _, contextMappings in pairs(mappings) do
-      for mode, keymaps in pairs(contextMappings) do
-        for _, keymap in ipairs(keymaps) do
-          local key = keymap[1]
-          local action = keymap[2]
-          local opts = { desc = keymap.desc, silent = true }
-          vim.keymap.set(mode, key, action, opts)
-        end
+    for mode, keymaps in pairs(mappings) do
+      for _, keymap in ipairs(keymaps) do
+        vim.keymap.set(mode, keymap[1], keymap[2], { desc = keymap.desc, silent = true })
       end
     end
   end,
   opts = {
-    preset = 'modern',
+    preset = 'classic',
     icons = {
       mappings = false,
     },
